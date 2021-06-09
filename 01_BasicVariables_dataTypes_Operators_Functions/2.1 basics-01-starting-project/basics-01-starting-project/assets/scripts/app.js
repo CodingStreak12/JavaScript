67941e0a84a2ee29b09bfb94cf2661c2c50093e1
CodingStreak12 <nitinww53@gmail.com>
2021-06-09 16:14:44 -0400

    This is the commit where I completed datatypes in javascritp.

--- 01_BasicVariables_dataTypes_Operators_Functions/2.1 basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ 01_BasicVariables_dataTypes_Operators_Functions/2.1 basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 let currentResult = 0;
-
+calculationDescription =  currentResult + ' + 10'
 currentResult = currentResult + 10;
 
-outputResult(currentResult, '');
+
+outputResult(currentResult, calculationDescription);
