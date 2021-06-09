8092e6cabd45d494001e34733f976742e951cf3d
CodingStreak12 <nitinww53@gmail.com>
2021-06-09 16:29:33 -0400

    Completed Functions

--- 01_BasicVariables_dataTypes_Operators_Functions/2.1 basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ 01_BasicVariables_dataTypes_Operators_Functions/2.1 basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -1,6 +1,14 @@
 let currentResult = 0;
+function add(num1, num2){
+    const result = num1 + num2;
+    alert("The output is " + result)
+
+}
+add(1, 2);
+add(2, 3);
 calculationDescription =  currentResult + ' + 10'
 currentResult = currentResult + 10;
+
 
 
 outputResult(currentResult, calculationDescription);
