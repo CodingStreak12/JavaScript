0be87831a1fb08dc2016cd164a9b445c45107722
CodingStreak12 <nitinww53@gmail.com>
2021-06-13 08:19:32 -0400

    Calculator app using javascript beginning with functions.

--- 01_BasicVariables_dataTypes_Operators_Functions/2.1 basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ 01_BasicVariables_dataTypes_Operators_Functions/2.1 basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -1,13 +1,39 @@
 let currentResult = 0;
-function add(num1, num2){
-    const result = num1 + num2;
-    return result;
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+}
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const description = currentResult + " + " + enteredNumber;
+    
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, description);
 
 }
+function multiply(){
+    const enteredNumber = getUserNumberInput();
+    const description = currentResult + " * " + enteredNumber;
+    currentResult = currentResult * enteredNumber;
+    outputResult(currentResult, description);
+}
+function subtract(){
+    const enteredNumber = getUserNumberInput();
+    const description = currentResult + " - " + enteredNumber;
+    currentResult = currentResult - enteredNumber;
+    outputResult(currentResult, description);
+}
+function divide(){
+    const enteredNumber = getUserNumberInput();
+    const description = currentResult + " / " + enteredNumber;
+    currentResult = currentResult / enteredNumber;
+    outputResult(currentResult, description);
+}
+addBtn.addEventListener('click', add);
+multiplyBtn.addEventListener('click', multiply);
+subtractBtn.addEventListener('click', subtract);
+divideBtn.addEventListener('click', divide);
 
-calculationDescription =  currentResult + " + " + add(4, 2);
-currentResult = add(4,2);
 
 
 
-outputResult(currentResult, calculationDescription);
+
