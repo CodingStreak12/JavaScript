0b0ad17f17b72f4f36de50af1d995b5c5b6e191f
CodingStreak12 <nitinww53@gmail.com>
2021-06-16 17:58:54 -0400

    Calculator app modified using conditional statements like if, else, else if

--- 01_BasicVariables_dataTypes_Operators_Functions/assets/scripts/app.js
+++ 01_BasicVariables_dataTypes_Operators_Functions/assets/scripts/app.js
@@ -1,68 +1,58 @@
 let currentResult = 0;
 let logEntries = [];
 
-function getUserNumberInput(){
-    return parseInt(userInput.value);
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
-function CreateAndWriteOutput(initialValue, operator, enteredValue){
-    const description = `${initialValue} ${operator} ${enteredValue}`;
-    outputResult(currentResult, description);
+function CreateAndWriteOutput(initialValue, operator, enteredValue) {
+  const description = `${initialValue} ${operator} ${enteredValue}`;
+  outputResult(currentResult, description);
 }
-function WriteToLog(
-    operator,
-    prevResult,
-    enteredNumber,
-    outputResult
-){
-    const logEntry = {
-        opeartor: operator,
-        prevResult: prevResult,
-        enteredNumber: enteredNumber,
-        outputResult: outputResult
-    };
-    logEntries.push(logEntry);
-    console.log(logEntries);
-    
-    
+
+function WriteToLog(operator, prevResult, enteredNumber, outputResult) {
+  const logEntry = {
+    opeartor: operator,
+    prevResult: prevResult,
+    enteredNumber: enteredNumber,
+    outputResult: outputResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
-function add(){
-    const enteredNumber = getUserNumberInput();
-    const initialNumber = currentResult; 
+function calculateResult(calculationType) {
+  const enteredNumber = getUserNumberInput();
+  const initialNumber = currentResult;
+  let mathOperator;
+  if (calculationType === "ADD") {
     currentResult = currentResult + enteredNumber;
-    CreateAndWriteOutput(initialNumber, "+" , enteredNumber);  
-    WriteToLog("ADD", initialNumber, enteredNumber, currentResult);
-
-}
-function multiply(){
-    const enteredNumber = getUserNumberInput();
-    const initialNumber = currentResult;
-    currentResult = currentResult * enteredNumber;
-    CreateAndWriteOutput(initialNumber, "*" , enteredNumber);
-    WriteToLog("MULITPLY", initialNumber, enteredNumber, currentResult);
-    
-}
-function subtract(){
-    const enteredNumber = getUserNumberInput();
-    const initialNumber = currentResult;
+    mathOperator = "+";
+  } else if (calculationType === "SUBTRACT") {
     currentResult = currentResult - enteredNumber;
-    CreateAndWriteOutput(initialNumber, "-" , enteredNumber);
-    WriteToLog("SUBTRACT", initialNumber, enteredNumber, currentResult);
-    
-}
-function divide(){
-    const enteredNumber = getUserNumberInput();
-    const initialNumber = currentResult;
+    mathOperator = "-";
+  } else if (calculationType === "MULTIPLY") {
+    currentResult = currentResult * enteredNumber;
+    mathOperator = "*";
+  } else if (calculationType === "DIVIDE") {
     currentResult = currentResult / enteredNumber;
-    CreateAndWriteOutput(initialNumber, "/" , enteredNumber);
-    WriteToLog("DIVIDE", initialNumber, enteredNumber, currentResult);
-    
+    mathOperator = "/";
+  }
+  CreateAndWriteOutput(initialNumber, mathOperator, enteredNumber);
+  WriteToLog(calculationType, initialNumber, enteredNumber, currentResult);
 }
-addBtn.addEventListener('click', add);
-multiplyBtn.addEventListener('click', multiply);
-subtractBtn.addEventListener('click', subtract);
-divideBtn.addEventListener('click', divide);
+function add() {
+  calculateResult("ADD");
+}
 
-
-
-
-
+function multiply() {
+  calculateResult("MULTIPLY");
+}
+function subtract() {
+  calculateResult("SUBTRACT");
+}
+function divide() {
+  calculateResult("DIVIDE");
+}
+addBtn.addEventListener("click", add);
+multiplyBtn.addEventListener("click", multiply);
+subtractBtn.addEventListener("click", subtract);
+divideBtn.addEventListener("click", divide);
